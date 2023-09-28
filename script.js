2573972d228727ced115920263ff9c9598a6aaea
Zeaan <59172904+Zeaan@users.noreply.github.com>
2023-09-28 16:08:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Researcher", "Developer", "Blogger"],
+        strings: ["Student", "Researcher", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student","Researcher", "Developer"],
+        strings: ["Student", "Researcher", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
